b6c1814055c56066f6530d5c4e7ac219a304e8ac
Ellen Adams <ellen@swarthmore.edu>
2020-06-01 14:30:37 -0400

    Add Publications

---
+++ src/components/Publications.js
@@ -0,0 +1,168 @@
+import React from 'react';
+import './stylesheets/publications.css'
+
+const Publications = () => {
+    return (
+       <div>
+          <h1 className = "title">Publications</h1>
+
+          <h3 className = "title">Neurotoxicology and Planarian Behavior</h3>
+
+            <p> D. Ireland, <under>V. Bochenek, D. Chaiken,</under> C. Rabeler, <under>S. Onoe</under>, A. Soni, and E.-M. S. Collins (2020), <italic>Dugesia japonica is the best suited of three planarian species for high-throughput toxicology screening</italic>.
+              Chemosphere, PMID: 32298908.
+              (<a href="https://www.biorxiv.org/content/10.1101/2020.01.23.917047v1.full.pdf" target="_blank">PDF</a>)
+            </p>
+
+            <p> <under>Z. Sabry, A. Ho</under>, D. Ireland, C. Rabeler, O. Cochet-Escartin, and E.-M. S. Collins (2019), <italic>Pharmacological or genetic targeting of Transient Receptor Potential (TRP) channels can disrupt the planarian escape response</italic>.
+             Plos ONE, PMID: 31805147.
+             (<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6894859/pdf/pone.0226104.pdf" target="blank">PDF</a>)
+            </p>
+
+            <p> S. Zhang, D. Ireland, N. Sipes, M. Behl, and E.-M. S. Collins (2019), <italic>Screening for neurotoxic potential of 15 ﬂame retardants using freshwater planarians</italic>. Neurotoxicology and Teratology, PMID: 30943442.
+            (<a href="https://www.biorxiv.org/content/10.1101/538280v1.full.pdf" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+          <h3 className = "title">Biomechanics</h3>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+----
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+----
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+---
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+            --
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+            <p>
+            (<a href="" target="blank">PDF</a>)
+            </p>
+
+       </div>
+    );
+}
+
+export default Publications;
